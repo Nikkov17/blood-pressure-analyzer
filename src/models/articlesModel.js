9ee384e1c8002302dd765e588c129af5a72f4a4c
mkavaliou <mikalai.kavaliou@burberry.com>
2019-04-17 12:55:37 +0300

    Revert "initial emplementation"
    This reverts commit daecbc757a959b32bf0f48bb636eb9cc70fe5e1a.

--- src/models/articlesModel.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-
-let articleScheme = new mongoose.Schema({
-    title: String,
-    text: String
-});
-
-module.exports = mongoose.model('articles', articleScheme);
